4ccb0806f0d36c8d2e92c773b7bfeef9fe46dd24
ASHUTOSH YADAV <97410328+AYDKIND@users.noreply.github.com>
2025-01-14 10:45:31 +0000

    03

---
+++ .devcontainer/01_basics/03_convertion.js
@@ -0,0 +1,17 @@
+// //let score = 33
+// let score = "Ashu"
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+//"33"=>33
+//"33sfg"=>NaN
+//true=>1; false=>0
+// let isLoggedIn =1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+let someNumber =33;
+let stringNumber=String(someNumber)
+console.log( typeof stringNumber);
