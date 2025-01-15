96bc673c5b24860f339f251e936aa26dfcbb608a
ASHUTOSH YADAV <97410328+AYDKIND@users.noreply.github.com>
2025-01-15 10:47:09 +0000

    ok

--- .devcontainer/01_basics/03_convertion.js
+++ .devcontainer/01_basics/03_convertion.js
@@ -12,6 +12,24 @@
 // let isLoggedIn =1;
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
-let someNumber =33;
-let stringNumber=String(someNumber)
-console.log( typeof stringNumber);
+// let someNumber =33;
+// let stringNumber=String(someNumber)
+// console.log( typeof stringNumber);
+// //operations
+// let value = 3;
+// let negValue =- value;
+// console.log(negValue);
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
