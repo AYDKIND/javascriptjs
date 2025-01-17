60eca8870771b50c9bbc684e9593b03001e0e079
ASHUTOSH YADAV <97410328+AYDKIND@users.noreply.github.com>
2025-01-17 10:47:27 +0000

    string

---
+++ .devcontainer/01_basics/06_string.js
@@ -0,0 +1,40 @@
+// const name ="Ashu";
+// const repoCount =50;
+// //   console.log(name+" "+repoCount+" "+"value")
+
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+// const gameName= new String(`Ashu`)
+
+// console.log(gameName[0]);  
+// console.log(gameName._proto_);  
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,2);
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8,3);
+// console.log(anotherString);
+
+
+const newStringOne = "Ashu kig and";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.google20.com"
+
+console.log(url.replace('20','-'))
+console.log(url.includes('"https://www.google20.com"'))
+
+console.log(newStringOne.split('*'));
+
+
+
