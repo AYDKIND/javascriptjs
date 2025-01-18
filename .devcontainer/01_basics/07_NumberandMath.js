089a588e8814d5d482ed6cb853a9c8d9b1961032
ASHUTOSH YADAV <97410328+AYDKIND@users.noreply.github.com>
2025-01-18 12:47:01 +0000

    number

---
+++ .devcontainer/01_basics/07_NumberandMath.js
@@ -0,0 +1,20 @@
+const score = 400;
+console.log(score);
+
+
+const balance = new Number(100.00);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1).length);
+
+
+const othernumber= 123.89899
+console.log(othernumber.toPrecision(2));
+
+
+const hundreds = 1000000000;
+console.log(hundreds.toLocaleString(en_in));
+
+
+
+
